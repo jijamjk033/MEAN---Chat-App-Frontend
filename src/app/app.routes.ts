52da541d79ce0fe1db27e83250404b25fd5924ba
jijamjk033 <jijamjk333@gmail.com>
2025-02-10 14:50:00 +0530

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomePageComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegistrationComponent },
+    
+];
